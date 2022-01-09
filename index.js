21ca793f408a595c37224ff7cfd71d3e6f5776e7
kamesuke42 <kamesuke42@kamesuke42.com>
2022-01-09 22:42:04 +0900

    [main] :+1: response text to html

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const listener = Deno.listen({ port: 8000 })
 console.log("http://localhost:8000");
 
 const turtle = `
+<html>
+<head>
+<title>kame.deno.dev</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+<body>
+かめだです。<br>
+<pre>
+<code style="font-size: 0.7em;">
                              ___-------___
                          _-~~             ~~-_
                       _-~                    /~-_
@@ -17,12 +27,16 @@ const turtle = `
        ~-----||====/~     |==================|       |/~~~~~
         (_(__/  ./     /                    \\_\\      \\.
                (_(___/                         \\_____)_)
+</code>
+</pre>
+</body>
+</html>
 `
 
 async function handle(conn) {
-    const requests = Deno.serveHttp(conn);
-    for await (const { respondWith } of requests) {
-        respondWith(new Response(turtle));
+  const requests = Deno.serveHttp(conn);
+  for await (const {respondWith} of requests) {
+        respondWith(new Response(new TextEncoder().encode(turtle)));
     }
 }
 
